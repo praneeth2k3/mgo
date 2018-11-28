dc1928d5c9106edd44727d6de80199658d0aa2a7
Praneeth Patil <praneeth.patil@vungle.com>
2018-11-27 16:16:37 -0800

    setup.sh cleanup

--- harness/setup.sh
+++ harness/setup.sh
@@ -28,8 +28,8 @@ start() {
     echo "Starting $COUNT processes..."
     for i in $(seq 30); do
         UP=$(svstat daemons/* | grep ' up ' | grep -v ' [0-3] seconds' | wc -l)
-        echo "$UP processes up..."
-        if [ x$COUNT == x$UP ]; then
+        echo "$UP/$COUNT processes up..."
+        if [ "x$COUNT" == "x$UP" ]; then
             echo "Running init.js with mongo..."
             mongo --nodb ../harness/mongojs/init.js
             exit 0
@@ -62,7 +62,7 @@ stop() {
             while true; do
                 DOWN=$(svstat daemons/* | grep 'supervise not running' | wc -l)
                 echo "$DOWN/$COUNT processes down..."
-                if [ x$DOWN == x$COUNT ]; then
+                if [ "x$DOWN" == "x$COUNT" ]; then
                     break
                 fi
                 sleep 1
